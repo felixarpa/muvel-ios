cd57cdf3fdeca0887baa5a577f25a877135e4116
Felix Arribas Pardo <felix.arribas@skyscanner.net>
2022-03-31 19:50:27 +0200

    Initial Commit

---
+++ Muvel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Muvel
+//
+//  Created by Felix Arribas on 31/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Muvel/MuvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuvelApp.swift
+//  Muvel
+//
+//  Created by Felix Arribas on 31/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MuvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
